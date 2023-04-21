bdcf6e8946b14d130aa685b189dc2a29b97fcdcd
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-04-21 17:19:03 -0500

    gtk4/libadwaita boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# A GTK4/Libadwaita GroupMe Client using the GroupyAPI Library
+""" A GTK4/Libadwaita GroupMe Client using the GroupyAPI Library """
+import sys
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
+
+
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.set_title("Groupings")
+
+        
+
+        
+
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp(application_id="io.github.thekrafter.Groupings")
+app.run(sys.argv)
