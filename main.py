c889053737bffa96a073d0da87b383711f0c5394
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-05-04 22:28:01 -0500

    first part of leaflet

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw
 
-from groupy.client import Client
+from grouppy import GroupMeClient
 import keyring
 
 
@@ -20,89 +20,86 @@ class MainWindow(Gtk.ApplicationWindow):
         super().__init__(*args, **kwargs)
         
         self.set_title("Groupings")
+        self.set_default_size(480, 720)
+
+        self.header = Adw.HeaderBar.new()
+        self.set_titlebar(self.header)
+
+        # Establish Client
         self.client_id = "BNDU2FYRc9qOJOenhpVYB3SpIIPr7PJE8PNzBkriPOxiFw3Z"
-        self.set_default_size(720, 240)
 
         logger.info(f'Fetching access token for {os.environ["USER"]}...')
         # Fetch Keyring
         keyring.get_keyring()
         self.access_token = keyring.get_password("io.github.thekrafter.Groupings", os.environ["USER"])
+        # Establish Client
         if self.access_token == None:
-            # Get Access Token if it Doesn't Exist
-            # ------------------------------------
-            # 0. Show we're waiting for token
-            # 1. Sends the user to the OAuth URL in their browser
-            # 2. Sets up a flask server to accept the callback
-            # 3. Gets the token from the passed 'access_token' argument, saving to to the keyring
-            # 4. Waits for the value to be in the keyring
-            # 5. Closes the webserver and continues
-
             # Inform user we're waiting for token
             self.waiting_screen = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-            self.waiting_label = Gtk.Label(label='Waiting for browser authentication...')
+            self.waiting_label = Gtk.Label(label='<strong>Waiting for browser authentication...</strong>')
             self.waiting_screen.append(self.waiting_label)
             self.set_child(self.waiting_screen)
 
-            import webbrowser
-            from flask import Flask, request
-            from multiprocessing import Process
-            
-            # Open in Browser
-            webbrowser.open(f'https://oauth.groupme.com/oauth/authorize?client_id={self.client_id}')
-
-            # Define Webserver
-            html_success_page = f"""
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>{self.get_title()} Authentication</title>
-                </head>
-                <body style="font-family:Cantarell,Sans-serif;padding-top:30px;">
-                    <center>
-                        <h3><strong>Success!</strong></h3>
-                        <h4>You may now close this window.</h4>
-                    </center>
-                </body>
-            <html>
-            """
-            self.flask_server = Flask(__name__)
-            @self.flask_server.route('/oauth', methods=['GET'])
-            def oauth():
-                args = request.args
-                keyring.set_password("io.github.thekrafter.Groupings", os.environ["USER"], args.get('access_token'))
-                return html_success_page
-        
-            # Start Webserver
-            webserver = Process(target=self.flask_server.run, kwargs=dict(host='127.0.0.1', port='8089'))
-            webserver.start()
-
-            # Wait for oauth to complete
-            waited = 0
-            while keyring.get_password("io.github.thekrafter.Groupings", os.environ["USER"]) == None:
-                time.sleep(5)
-                waited += 5
-                logger.debug(f'Waiting for access token... ({waited} seconds)')
-            
-            # Once complete, tidy up
-            self.access_token = keyring.get_password("io.github.thekrafter.Groupings", os.environ["USER"])
-            logger.success(f'Got access token!')
-            webserver.kill()
-            webserver.join()
-        
+            logger.debug(f'No token found. Logging in...')
+            self.client = GroupMeClient(self.client_id, oauth_complete=False, app_name="Groupings")
+            self.client.authenticate()
+            logger.success(f'Logged in!')
+            keyring.get_keyring()
+            keyring.set_password("io.github.thekrafter.Groupings", os.environ["USER"], self.client.access_token)
+            logger.info(f'Set new access token on keyring.')
         else:
             logger.success(f'Found access token!')
+            self.client = GroupMeClient(self.access_token, oauth_complete=True)
 
-        # Establish Client
-        self.client = Client.from_token(self.access_token)
+
 
         # Construct window
-        self.box_grouplist = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.set_child(self.box_grouplist)
-        ## Add group items
-        self.listbox_groups = Gtk.ListBox()
-        for group in client.groups.list():
-            current = Gtk.ListBoxRow(label=group.name, description=group.id)
-            self.listbox_groups.append(current)
+        ## Leaflet
+        self.chat_leaflet = Adw.Leaflet.new() # can_navigate_back = True, hexpand = True
+        self.chat_leaflet.set_can_navigate_back(True)
+        self.chat_leaflet.set_hexpand(True)
+        
+        ### Left Pane
+        self.chat_leaflet_left = Gtk.Box()
+        #### HeaderBar
+        #groups_headerbar = Adw.HeaderBar.new()
+        #groups_headerbar.set_title_widget(Gtk.Label.new("Groups"))
+        #self.chat_leaflet_left.append(groups_headerbar)
+        #### ListBox
+        self.groups_listing = Gtk.ListBox.new()
+        self.groups_listing.set_selection_mode(Gtk.SelectionMode.SINGLE)
+        self.groups_listing.set_hexpand(True)
+        self.groups_listing.set_margin_top(12)
+        self.groups_listing.set_margin_bottom(12)
+        self.groups_listing.set_margin_start(6)
+        self.groups_listing.set_margin_end(6)
+
+        ##### Get Groups
+        for group in self.client.get_groups():
+            current = Adw.ActionRow.new()
+            current.set_title(f'{group["name"]}')
+            current.set_subtitle(f'{group["id"]}')
+            current.connect('activated', self.on_group_open, group["id"])
+            self.groups_listing.append(current)
+        self.chat_leaflet_left.append(self.groups_listing)
+
+        self.chat_leaflet.append(self.chat_leaflet_left)
+        #self.box_grouplist = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        #self.chat_leaflet.append(self.box_grouplist)
+        ### Add group items
+        #for group in self.client.get_groups():
+        #    current = Gtk.Button.new_with_label(f'{group["name"]}')
+        #    current.set_margin_top(5)
+        #    current.set_margin_bottom(5)
+        #    current.set_margin_start(5)
+        #    current.set_margin_end(5)
+        #    self.box_grouplist.append(current)
+        
+        self.set_child(self.chat_leaflet)
+
+    def on_group_open(self, group_id):
+        """ Open group panel """
+        logger.info(f'Open group {group_id}')
     
     
 
