dd4735a162a5bd94e09544db9fed931b64b873d7
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2024-03-05 22:38:18 -0600

    spoof headers when fetching resources so groupme likes us

--- src/groupings/internal/cache.py
+++ src/groupings/internal/cache.py
@@ -15,8 +15,13 @@ from typing import Optional, Union
 from .lang import lang
 
 GROUPME_IMAGEURL_PREFIX = 'https://i.groupme.com/'
+SPOOFED_HEADERS = [
+    ('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'),
+    ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'),
+    ('Accept-Encoding', 'gzip, deflate, br'),
+]
 
-def fetch(token: str, url: Optional[str]) -> Optional[Union[str, bool]]:
+def fetch(token: str, url: Optional[str]) -> Optional[Union[str, bool]]: #TODO: Doesn't need a token (?)
     """ Fetch a file from GroupMe's image server.
     Returns path to locally cached version of file. If file URL is invalid, returns None.
     If HTTP was unsuccessful, returns False """
@@ -31,7 +36,7 @@ def fetch(token: str, url: Optional[str]) -> Optional[Union[str, bool]]:
         else:
             try:
                 opener = request.build_opener()
-                opener.addheaders = [('X-Access-Token', token)]
+                opener.addheaders = SPOOFED_HEADERS
                 request.install_opener(opener)
                 path, headers = request.urlretrieve(url, cache_file_path)
                 return path
